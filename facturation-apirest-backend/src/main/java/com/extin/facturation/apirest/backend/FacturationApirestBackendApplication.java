5e4f96f9ddb80705995b4b8e64ca4bec3bb190be
John Duarte <johnduarte7@gmail.com>
2019-12-04 01:21:31 -0500

    Se crea proyecto back end

---
+++ facturation-apirest-backend/src/main/java/com/extin/facturation/apirest/backend/FacturationApirestBackendApplication.java
@@ -0,0 +1,13 @@
+package com.extin.facturation.apirest.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FacturationApirestBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FacturationApirestBackendApplication.class, args);
+	}
+
+}
---
+++ facturation-apirest-backend/src/test/java/com/extin/facturation/apirest/backend/FacturationApirestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.extin.facturation.apirest.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacturationApirestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
